0cd7e4c79138ec675b79b8ba694b98b98f46a99d
Carlos César Harris Castillo <47110071+ccharris940406@users.noreply.github.com>
2024-04-27 10:36:42 -0400

    Update page.test.tsx
    testing for fail tests.

--- src/__tests__/page.test.tsx
+++ src/__tests__/page.test.tsx
@@ -2,7 +2,7 @@ import { expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
 
 function TestingVitestConf() {
-  return <h1>Hello World!</h1>;
+  return <h1>Hello</h1>;
 }
 
 test("Testing vitest config", () => {
